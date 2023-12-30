e482255ef2bf8a47c8ae80108e77150b2090be38
AlexseiT <78640047+AlexseiT@users.noreply.github.com>
2023-12-30 14:30:15 +0700

    Add files via upload

---
+++ projectDICE/scripts/main.js
@@ -0,0 +1,42 @@
+﻿class Randomizer
+{
+    constructor() {}
+    GetRandom(numberMin,numberMax)
+    {
+        return Math.floor(Math.random() * (numberMax - numberMin)) + numberMin;
+    }
+    Visualize() {}
+    CheckCount() {}
+}
+class Dice extends Randomizer
+{
+    constructor()
+    {
+        super();
+        this.numberRand = super.GetRandom(1, 6);
+        this.src = "assets/images/dice/" + this.numberRand + ".png";
+        this.image = new Image();
+        this.image.src = this.src;
+    }
+    CheckCount()
+    {
+        return document.getElementById("dicePlace").getElementsByTagName("img").length;
+    }
+    Visualize()
+    {
+        console.log(this.CheckCount());
+        if (this.CheckCount() > 49) document.getElementById('dicePlace').innerHTML = "";
+        document.getElementById('dicePlace').appendChild(this.image);
+    }
+}
+class Card extends Randomizer
+{
+    constructor() {}
+    CheckCount() {}
+    Visualize() {}
+}
+function OnClickButtonRoll()
+{
+    render = new Dice();
+    render.Visualize();
+}
