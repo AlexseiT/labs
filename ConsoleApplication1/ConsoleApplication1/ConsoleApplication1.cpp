3f9dec67ba12c55e83853b89c3d607c6c2d344ea
AlexseiT <78640047+AlexseiT@users.noreply.github.com>
2023-12-30 15:03:36 +0700

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,10 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	float  x;
+	x = 1 + (27 - 1)%9 ;
+	printf("%f", x);
+}
+
---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+	//BFS - очереди
+	int n, m;
+	ios_base::sync_with_stdio(false);
+	cin >> n >> m;
+	vector<int> used(123456, 0);
+	queue<int> q;
+	q.push(n);
+	//q.size();
+	//q.empty();
+	used[n] = 1;
+	while (!q.empty()) {
+		int tmp = q.front();
+		q.pop();
+		if (tmp * 2 < 1e4 && !used[tmp * 2]) {
+			q.push(tmp * 2);
+			used[tmp * 2] = used[tmp] + 1;
+		}
+		if (!used[tmp - 1] && tmp - 1 > 0) {
+			q.push(tmp - 1);
+			used[tmp - 1] = used[tmp] + 1;
+		}
+	}
+	cout << used[m] - 1;
+}
+
