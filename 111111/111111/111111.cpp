b096e418784b6ade6a78a45b52c6177e40c33b76
AlexseiT <78640047+AlexseiT@users.noreply.github.com>
2023-12-30 15:01:04 +0700

    Add files via upload

---
+++ 111111/111111/111111.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+int main() {
+	int a;
+	scanf_s("%d", &a);
+	if (a % 2 == 0 && a!=2)  printf("YES");
+	else printf("NO");
+}
---
+++ 222222/222222/222222.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+int main() {
+	int n, m, a;
+	scanf_s("%d %d %d", &n , &m , &a);
+	if (a % 2 == 0 && a != 2)  printf("YES");
+	else printf("NO");
+}
