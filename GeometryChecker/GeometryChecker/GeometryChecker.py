5b8967d700b63b929abd8a28f02bf35d037a2e25
AlexseiT <78640047+AlexseiT@users.noreply.github.com>
2023-12-30 14:05:44 +0700

    Add files via upload

---
+++ GeometryChecker/GeometryChecker/GeometryChecker.py
@@ -0,0 +1,10 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation, BatchNormalization, AveragePooling2D
+from tensorflow.keras.optimizers import SGD, RMSprop, Adam
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import logging
+import numpy as np
+import time
---
+++ formatcode/formatcode/formatcode.cpp
@@ -0,0 +1,43 @@
+﻿void temp(
+        vector<double>& elements,
+        const char* input_char,
+        int position_of_element)
+{
+    string str = input_char;
+    if (check_strock(str) == 1 && !str.empty()) {
+        double input_element = stod(str);
+        elements.resize(3);
+        switch (position_of_element) {
+        case 0: {
+            elements[0] = input_element;
+            elements[1] = Kelvin_Celsius(input_element, false);
+            elements[2] = In_Fahrenheit(input_element, false);
+            break;
+        }
+        case 1: {
+            elements[0] = Kelvin_Celsius(input_element, true);
+            elements[1] = input_element;
+            elements[2] = In_Fahrenheit(input_element, true);
+            break;
+        }
+        case 2: {
+            elements[0] = From_Fahrenheit(input_element, false);
+            ;
+            elements[1] = From_Fahrenheit(input_element, true);
+            elements[2] = input_element;
+            break;
+        }
+        }
+    }
+}
+
+CTEST(Check, check_strock_true)
+{
+    for (double input = 0.01; input <= 5; input = input + 0.01) {
+        std::string str;
+        str += std::to_string(input);
+        char i = check_strock(str);
+        ASSERT_EQUAL(1, i);
+        str.clear()
+    }
+}
